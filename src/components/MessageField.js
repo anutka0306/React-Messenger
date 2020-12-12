c8a2747e991219ad159c515810308a6808d00db7
anutka0306 <anutka0306@mail.ru>
2020-12-12 17:41:59 +0300

    author added

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -5,18 +5,19 @@ export default class MessageField extends React.Component{
 
     state = {
       messages: ["Hello", "How are you?"],
+        author: ["User", "User"],
         answers : ["HI! I am Robot", "I'll be back", "How was your day?", "Do you like ice-cream?","Where are you from?"],
     };
 
     handleClick = () =>{
-        this.setState({messages: [...this.state.messages, "I am Good"]});
+        this.setState({messages: [...this.state.messages, "I am Good"], author: [...this.state.author, "User"]});
     };
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if(this.state.messages.length % 2 === 1){
             let rand = Math.floor(Math.random() * this.state.answers.length);
             setTimeout(() =>
-                this.setState({messages: [...this.state.messages, this.state.answers[rand]]}),
+                this.setState({messages: [...this.state.messages, this.state.answers[rand]], author: [...this.state.author, 'Robot']}),
                     1000
             );
         }
@@ -24,7 +25,7 @@ export default class MessageField extends React.Component{
 
     render() {
         const messageElements = this.state.messages.map((text, index) => (
-            <Message text={text} key={index}/>
+            <Message text={text} author={this.state.author[index]} key={index}/>
         ));
 
         return (

e65ea233aa657df8075d77ddafb8c818610601cc
anutka0306 <anutka0306@mail.ru>
2020-12-21 17:06:00 +0300

    userReducer

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import Menu from "./Menu";
-
+import PropTypes from 'prop-types';
 import '../styles/styles.css';
+import User from "../components/User/User";
+import { bindActionCreators } from "redux";
+import connect from "react-redux/es/connect/connect";
 
-export default class Profile extends React.Component{
+class Profile extends React.Component{
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+    };
+
     render() {
         return (
             <div className='main-layout'>
                 <div className='content-wrapper'>
                     <h3>Profile</h3>
+                    <User name={this.props.name}/>
                 </div>
             </div>
         );
     }
 }
+
+const mapStateToProps = ({ userReducer }) =>({
+    user: userReducer.user,
+});
+
+const mapDispatchToProps = dispatch => (bindActionCreators({}, dispatch));
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
